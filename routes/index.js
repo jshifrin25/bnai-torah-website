ab4373548573b497558ad16810b22015c4a8bced
Zalman Shifrin <jshifrin@bnaitorahcleveland.info>
2017-06-07 03:06:36 +0000

    initial local commit for website

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Khal Bnai Torah Of Cleveland' });
+});
+
+module.exports = router;
